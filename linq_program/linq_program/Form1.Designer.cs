281a31ab233ce16b2022b88303384667ef8e2afd
Lessnaus <lessnau001@gmail.com>
2023-04-06 16:34:27 +0200

    wczytanie danych z pliku csv

--- linq_program/linq_program/Form1.Designer.cs
+++ linq_program/linq_program/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Wczytaj dane";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.letsClick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 151);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(662, 287);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(684, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- linq_program/linq_program/Form1.cs
+++ linq_program/linq_program/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 namespace linq_program
 {
@@ -15,6 +18,43 @@ namespace linq_program
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void letsClick(object sender, MouseEventArgs e)
+        {
+            
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "linq.csv");
+            
+            if (File.Exists(filePath))
+            {
+                DataTable dt = new DataTable();
+                string[] lines = File.ReadAllLines(filePath);
+                if (lines.Length > 1)
+                {
+                    string[] headers = lines[0].Split(',');
+                    int numColumns = headers.Length;
+                    for (int i = 0; i < numColumns; i++)
+                    {
+                        dt.Columns.Add(headers[i]);
+                    }
+                    for (int i = 1; i <= lines.Length - 1; i++)
+                    {
+                        string[] lineData = lines[i].Split(',');
+                        DataRow row = dt.NewRow();
+                        for (int j = 0; j < numColumns; j++)
+                        {
+                            row[j] = lineData[j];
+                        }
+                        dt.Rows.Add(row);
+                    }
+                    dataGridView1.DataSource = dt;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
