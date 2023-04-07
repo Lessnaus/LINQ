ba1a101ef14dfc6b619563701f4c5c3601a4ee39
Lessnaus <lessnau001@gmail.com>
2023-04-07 10:58:57 +0200

    Update

--- linq_program/linq_program/Form1.Designer.cs
+++ linq_program/linq_program/Form1.Designer.cs
@@ -31,50 +31,265 @@
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.Filtr = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Sortowanie = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Wybierz = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Filtr.SuspendLayout();
+            this.Sortowanie.SuspendLayout();
+            this.Wybierz.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(275, 67);
+            this.button1.Location = new System.Drawing.Point(283, 161);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(131, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Wczytaj dane";
+            this.button1.Text = "Wczytaj wszystkie dane";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.letsClick);
+            this.button1.Click += new System.EventHandler(this.letsClick);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(10, 151);
+            this.dataGridView1.Location = new System.Drawing.Point(10, 190);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(662, 287);
+            this.dataGridView1.Size = new System.Drawing.Size(662, 322);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.Visible = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(455, 66);
+            this.button2.Location = new System.Drawing.Point(133, 56);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(76, 53);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "Filtruj";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Imie:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(6, 32);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(6, 72);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 8;
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(133, 56);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(76, 53);
+            this.buttonSort.TabIndex = 9;
+            this.buttonSort.Text = "Sortuj";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "profession"});
+            this.comboBox4.Location = new System.Drawing.Point(6, 32);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 10;
+            // 
+            // Filtr
+            // 
+            this.Filtr.Controls.Add(this.label3);
+            this.Filtr.Controls.Add(this.label2);
+            this.Filtr.Controls.Add(this.comboBox2);
+            this.Filtr.Controls.Add(this.comboBox3);
+            this.Filtr.Controls.Add(this.button2);
+            this.Filtr.Controls.Add(this.comboBox1);
+            this.Filtr.Controls.Add(this.label1);
+            this.Filtr.Location = new System.Drawing.Point(12, 12);
+            this.Filtr.Name = "Filtr";
+            this.Filtr.Size = new System.Drawing.Size(215, 143);
+            this.Filtr.TabIndex = 11;
+            this.Filtr.TabStop = false;
+            this.Filtr.Text = "Filtr";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nazwisko:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Stanowisko:";
+            // 
+            // Sortowanie
+            // 
+            this.Sortowanie.Controls.Add(this.label5);
+            this.Sortowanie.Controls.Add(this.comboBox5);
+            this.Sortowanie.Controls.Add(this.label4);
+            this.Sortowanie.Controls.Add(this.comboBox4);
+            this.Sortowanie.Controls.Add(this.buttonSort);
+            this.Sortowanie.Location = new System.Drawing.Point(242, 12);
+            this.Sortowanie.Name = "Sortowanie";
+            this.Sortowanie.Size = new System.Drawing.Size(215, 143);
+            this.Sortowanie.TabIndex = 12;
+            this.Sortowanie.TabStop = false;
+            this.Sortowanie.Text = "Sortowanie";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Wedłóg:";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "profession"});
+            this.comboBox5.Location = new System.Drawing.Point(6, 72);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Jak:";
+            // 
+            // Wybierz
+            // 
+            this.Wybierz.Controls.Add(this.textBox2);
+            this.Wybierz.Controls.Add(this.textBox1);
+            this.Wybierz.Controls.Add(this.label6);
+            this.Wybierz.Controls.Add(this.label7);
+            this.Wybierz.Controls.Add(this.button3);
+            this.Wybierz.Location = new System.Drawing.Point(463, 12);
+            this.Wybierz.Name = "Wybierz";
+            this.Wybierz.Size = new System.Drawing.Size(215, 143);
+            this.Wybierz.TabIndex = 14;
+            this.Wybierz.TabStop = false;
+            this.Wybierz.Text = "Wybierz po id";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Do:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Od:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(133, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 53);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Sortuj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 450);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(686, 524);
+            this.Controls.Add(this.Wybierz);
+            this.Controls.Add(this.Sortowanie);
+            this.Controls.Add(this.Filtr);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "LINQ";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Filtr.ResumeLayout(false);
+            this.Filtr.PerformLayout();
+            this.Sortowanie.ResumeLayout(false);
+            this.Sortowanie.PerformLayout();
+            this.Wybierz.ResumeLayout(false);
+            this.Wybierz.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -83,6 +298,25 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.GroupBox Filtr;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox Sortowanie;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.GroupBox Wybierz;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- linq_program/linq_program/Form1.cs
+++ linq_program/linq_program/Form1.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+
+
 
 namespace linq_program
 {
@@ -22,10 +18,100 @@ namespace linq_program
 
         private void letsClick(object sender, MouseEventArgs e)
         {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
             dataGridView1.Visible = true;
             string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "linq.csv");
 
-            
+           
+
+            if (File.Exists(filePath))
+            {
+                DataTable dt = new DataTable();
+                var lines = File.ReadLines(filePath).Select(line => line.Split(','));
+                var headers = lines.First();
+                int numColumns = headers.Length;
+                foreach (var header in headers)
+                {
+                    dt.Columns.Add(header);
+                }
+                string filter1 = comboBox1.Text;
+                string filter2 = comboBox2.Text;
+                string filter3 = comboBox3.Text;
+                label1.Text = filter1+ filter2;
+                var data = lines.Skip(1)
+     .Where(fields => (string.IsNullOrEmpty(filter2) || fields[1] == filter2) &&
+                      (string.IsNullOrEmpty(filter1) || fields[5] == filter1) &&
+                      (string.IsNullOrEmpty(filter3) || fields[2] == filter3));
+
+                foreach (var line in data)
+                {
+                    DataRow row = dt.NewRow();
+                    for (int i = 0; i < numColumns; i++)
+                    {
+                        row[i] = line[i];
+                    }
+                    dt.Rows.Add(row);
+                }
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "linq.csv");
+            if (File.Exists(filePath))
+            {
+                var lines = File.ReadAllLines(filePath);
+                var headers = lines[0].Split(',');
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    var values = lines.Skip(1).Select(l => l.Split(',')[i]).Distinct().OrderBy(v => v);
+                    if (i == 5)
+                    {
+                        comboBox1.Items.AddRange(values.ToArray());
+                    }
+                    else if (i == 1)
+                    {
+                        comboBox2.Items.AddRange(values.ToArray());
+                    }
+                    else if (i == 2)
+                    {
+                        comboBox3.Items.AddRange(values.ToArray());
+                    }
+                    // Dodaj więcej ComboBox-ów dla innych kolumn, jeśli jest to konieczne.
+                }
+            }
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            string sortColumnName = comboBox4.SelectedItem.ToString();
+
+            var sortedData = dataGridView1.Rows.Cast<DataGridViewRow>()
+                                     .OrderBy(r => r.Cells[sortColumnName].Value)
+                                     .ToList();
+
+            dataGridView1.Rows.Clear();
+            foreach (var row in sortedData)
+            {
+                dataGridView1.Rows.Add(row);
+            }
+        }
+
+        private void letsClick(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "linq.csv");
+
+
             if (File.Exists(filePath))
             {
                 DataTable dt = new DataTable();
@@ -48,39 +134,12 @@ namespace linq_program
                 }
                 dataGridView1.DataSource = dt;
             }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-            dataGridView1.Visible = true;
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "linq.csv");
-            if (File.Exists(filePath))
-            {
-                DataTable dt = new DataTable();
-                var lines = File.ReadLines(filePath).Select(line => line.Split(','));
-                var headers = lines.First();
-                int numColumns = headers.Length;
-                foreach (var header in headers)
-                {
-                    dt.Columns.Add(header);
-                }
-                var data = lines.Skip(1).Where(fields => fields[5] == "developer");
-                foreach (var line in data)
-                {
-                    DataRow row = dt.NewRow();
-                    for (int i = 0; i < numColumns; i++)
-                    {
-                        row[i] = line[i];
-                    }
-                    dt.Rows.Add(row);
-                }
-                dataGridView1.DataSource = dt;
-            }
+
         }
     }
 }
